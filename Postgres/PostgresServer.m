d0e8a36cfbb40f988e5d1bb547166791ac3d834d
Michael Weisman <mweisman@gmail.com>
2013-10-17 11:33:02 -0700

    Create postgres role for the cli adverse

--- Postgres/PostgresServer.m
+++ Postgres/PostgresServer.m
@@ -132,15 +132,23 @@ static NSString * PGNormalizedVersionStringFromString(NSString *version) {
         [self executeCommandNamed:@"initdb" arguments:[NSArray arrayWithObjects:[NSString stringWithFormat:@"-D%@", _varPath], [NSString stringWithFormat:@"-E%@", @"UTF8"], [NSString stringWithFormat:@"--locale=%@_%@.UTF-8", [[NSLocale currentLocale] objectForKey:NSLocaleLanguageCode], [[NSLocale currentLocale] objectForKey:NSLocaleCountryCode]], nil] terminationHandler:^(NSUInteger status) {
             [self executeCommandNamed:@"pg_ctl" arguments:[NSArray arrayWithObjects:@"start", [NSString stringWithFormat:@"-D%@", _varPath], @"-w", [NSString stringWithFormat:@"-o'-p%ld'", port], @"-l", [NSString stringWithFormat:@"%@/../postgis_log.txt", _varPath], nil] terminationHandler:^(NSUInteger status) {
                 [self executeCommandNamed:@"createdb" arguments:[NSArray arrayWithObjects:[NSString stringWithFormat:@"-p%ld", port], NSUserName(), nil] terminationHandler:^(NSUInteger status) {
-                    // Install PostGIS in deault DB
-                    [self executeCommandNamed:@"psql" arguments:[NSArray arrayWithObjects:[NSString stringWithFormat:@"-p%ld", port], @"-w", @"-c CREATE EXTENSION postgis", nil] terminationHandler:^(NSUInteger status) {
-                        if (completionBlock) {
-                            completionBlock(status);
-                        }
+                    // Setup "postgres" user so pgAdmin works out of the box for those who insist on using it
+                    [self executeCommandNamed:@"psql" arguments:[NSArray arrayWithObjects:[NSString stringWithFormat:@"-p%ld", port], @"-w", @"-c CREATE USER postgres WITH PASSWORD 'postgres'", nil] terminationHandler:^(NSUInteger status) {
+                        [self executeCommandNamed:@"psql" arguments:[NSArray arrayWithObjects:[NSString stringWithFormat:@"-p%ld", port], @"-w", @"-c ALTER USER postgres WITH SUPERUSER", nil] terminationHandler:^(NSUInteger status) {
+                            // Install PostGIS in default DB
+                            [self executeCommandNamed:@"psql" arguments:[NSArray arrayWithObjects:[NSString stringWithFormat:@"-p%ld", port], @"-w", @"-c CREATE EXTENSION postgis", nil] terminationHandler:^(NSUInteger status) {
+                                // Make sure sfcgal functions are installed
+                                [self executeCommandNamed:@"psql" arguments:@[[NSString stringWithFormat:@"-f %@/../share/contrib/postgis-2.1/sfcgal.sql",_binPath]] terminationHandler:^(NSUInteger status) {
+                                    if (completionBlock) {
+                                        completionBlock(status);
+                                    }
+                                 }];
+                            }];
+                        }];
                     }];
                 }];
             }];
-        }];    
+        }];
     } else {
         [self executeCommandNamed:@"pg_ctl" arguments:[NSArray arrayWithObjects:@"start", [NSString stringWithFormat:@"-D%@", _varPath], [NSString stringWithFormat:@"-o'-p%ld'", port], @"-l", [NSString stringWithFormat:@"%@/../postgis_log.txt", _varPath], nil] terminationHandler:^(NSUInteger status) {
             // Kill server and try one more time if server can't be started
